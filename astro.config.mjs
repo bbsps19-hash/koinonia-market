960a0c315823c88663c0fa16ee25de927fea3653
bbsps19 <bbsps19@gmail.com>
2026-06-09 16:33:35 +0000

    Build and deploy Koinonia Market site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://bbsps19-hash.github.io',
+  base: '/koinonia-market',
+});
